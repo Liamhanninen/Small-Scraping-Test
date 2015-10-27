52f1acd0535dd2208a4c3988b45c56b0fb5008ba
Liamhanninen <liam.hanninen@gmail.com>
2015-10-27 09:18:56 -0500

    Added comments

--- Scrapethis.py
+++ Scrapethis.py
@@ -9,18 +9,21 @@ import urllib2
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
 
-#def htmlgrab():
+
 url = 'http://post-gazette.com/local/breaking.more'
 r = requests.get(url)
 content = r.text
 soup = BeautifulSoup(content)
 linesoup = soup.find_all("div", class_="innertube10 hundred")
 stringsoup = str(linesoup)
+#The above lines grab/get url, parse it, and then turn it into a 
+#string so Python can do its thing
 
 
-#def write():
 with open ("scrapecrimedata3" + ".txt", "w+") as newfile:
     newfile.write(stringsoup)
+#The above lines create (if not created already) and write the 
+#parsed html to a .txt file
 
 
 def filtered():
@@ -29,6 +32,14 @@ def filtered():
             for line in newfile:
                 if "police" in line: filtered.write(line)
 ()
+'''
+The above lines search that .txt file for a specific keyword
+and then add any lines with those keywords to a new .txt file
+This is what I am trying to fix/improve;
+    I can only seem to search for a single keyword. In this case
+    "police" is the only string I can search for. Separating by
+    commas and/or using 'and' and 'or' did not seem to work.
+    '''
        
 filtered()       
 
